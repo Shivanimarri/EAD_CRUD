50ef6847743c5c93580a35e4b09bd063dc1ca73e
Shivanimarri <marrishivani23@gmail.com>
2024-09-26 20:12:25 +0530

    New Login page and styling

--- Server/entrypoint.js
+++ Server/entrypoint.js
@@ -6,7 +6,7 @@ const studentRouter = require('./controller/students')
 
 //const url =  'mongodb://localhost:27017/EAD'  
 //const url= 'mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=m101'
-const url= 'mongodb+srv://marrishivani23:9502624404@cbit.qkyho.mongodb.net/?retryWrites=true&w=majority&appName=cbit'
+const url= 'mongodb+srv://marrishivani23:9502624404@cbit.qkyho.mongodb.net/ead?retryWrites=true&w=majority&appName=cbit'
 const app = express()
 mongoose.connect(url)
 const con = mongoose.connection
@@ -16,10 +16,16 @@ con.on('open', () =>
 {
 console.log('connected...')
 })
-app.use(cors())
+const corsOptions = {
+    origin:"http://localhost:5173",
+    methods: "GET,POST,DELETE,PUT,PATCH,HEAD",
+    credentials:true
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 
-app.use('/',studentRouter)
+app.use('/student',studentRouter)
 app.listen(9000, () =>
 {
 console.log('Server started')
